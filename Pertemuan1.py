6d6ba623229e4bf07c4a0e53b6cbf4d5734e5ad7
tacosz <nabilnasruddin.offc@gmail.com>
2024-10-07 19:01:26 +0700

    Done

--- Pertemuan1.py
+++ Pertemuan1.py
@@ -1,1 +1,16 @@
 print ("Nabil Nasruddin Al Mutawakkil")
+print ("I am learning Python basics")
+
+#menanyakan nama user
+nama = input ("Siapa Nama Kamu? ")
+#menampilkan nama user yg telah di input ke dalam variabel nama
+print ("Hello",nama,"!")
+
+#petik tiga multi-line
+"""
+menanyakan umur user
+menampilkan umur user yg telah di input
+kedalam variabel age
+"""
+age = input('How old are you? ')
+print (age)
